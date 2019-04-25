8b84a3bf4c15175f4b13997e0bc9e588c916756c
Rolf <rolf.shroff@outlook.com>
2019-04-25 19:06:13 +1000

    Update MyRnDRunbook.ps1

--- MyRnDRunbook.ps1
+++ MyRnDRunbook.ps1
@@ -1,4 +1,1 @@
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection 
-Connect-AzureRmAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
-
-Get-AzureRmVM -ResourceGroupName rolf-rnd-rg -Name RDGw01
